1c81c37e4d9b6afc55fbc96582d145fcb6c20776
kpovel <kar.pavel123@gmail.com>
2022-02-01 22:39:35 +0200

    remade  showList

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const STATUSES = {
-    ToDo: 'To Do',
-    InProgress: 'In Progress',
-    Done: 'Done'
+    TODO: 'To Do',
+    IN_PROGRESS: 'In Progress',
+    DONE: 'Done'
 }
 
 const list = {
-    'create a new practice task': STATUSES.InProgress,
-    'make a bed': STATUSES.Done,
-    'write a post': STATUSES.ToDo
+    'create a new practice task': STATUSES.IN_PROGRESS,
+    'make a bed': STATUSES.DONE,
+    'write a post': STATUSES.TODO
 }
 
 function changeStatus(task, status) {
-    if (task in list && status in STATUSES) {
-        list[task] = STATUSES[status]
+    if (task in list) {
+        list[task] = status
     }
 }
 
 function addTask(task) {
     if (!task) return
-    list[task] = STATUSES.ToDo
+    list[task] = STATUSES.TODO
 }
 
 function deleteTask(task) {
@@ -26,31 +26,32 @@ function deleteTask(task) {
 }
 
 function showList() {
-    let counter = 0
-
-    for (let status in STATUSES) {
-        console.log(`${status}:`)
-        for (let task in list) {
-            if (list[task] === STATUSES[status]) {
-                console.log(` ${task}`)
-                counter++
-            }
-        }
-        if (counter === 0) {
-            console.log(' -')
-        }
-        counter = 0
+    const tasks = {
+        [STATUSES.TODO]: '',
+        [STATUSES.IN_PROGRESS]: '',
+        [STATUSES.DONE]: '',
     }
+
+    for (let key in list) {
+        tasks[list[key]] += ` ${key}\n `
+    }
+    console.log(
+        `${STATUSES.TODO}:
+ ${tasks[STATUSES.TODO] || ' -'}
+${STATUSES.IN_PROGRESS}:
+ ${tasks[STATUSES.IN_PROGRESS] || ' -'}
+${STATUSES.DONE}:
+ ${tasks[STATUSES.DONE] || ' -'} `)
 }
 
 addTask('complete the check')
 addTask('implement "—"')
 addTask('remake showList')
-changeStatus('remake showList', 'InProgress')
-changeStatus('implement "—"', 'Done')
-changeStatus('complete the check', 'Done')
+changeStatus('remake showList', STATUSES.IN_PROGRESS)
+changeStatus('implement "—"', STATUSES.DONE)
+changeStatus('complete the check', STATUSES.DONE)
 deleteTask('make a bed')
-changeStatus('implement "—"', 'ToDo')
+changeStatus('implement "—"', STATUSES.TODO)
 
 showList()
 
